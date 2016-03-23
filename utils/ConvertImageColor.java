efd4625b9202e12a8293779b0e61aa672d3fabbf
Aelous-6 <616521549@qq.com>
2016-03-23 14:35:02 +0800

    add some util java class

---
+++ utils/ConvertImageColor.java
@@ -0,0 +1,14 @@
+public class ConvertImageColor {
+    Activity activity;
+
+    public ConvertImageColor(Activity activity) {
+        this.activity = activity;
+    }
+
+    // 改变图标的颜色
+    public  Drawable changeImageColor(int drawable,int color){
+        Drawable myIcon = activity.getResources().getDrawable(drawable);
+        myIcon.setColorFilter(activity.getResources().getColor(color), PorterDuff.Mode.SRC_ATOP);
+        return  myIcon;
+    }
+}
